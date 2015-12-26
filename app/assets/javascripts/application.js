725213e520593080672ab1f7982b1ecfb2d594d2
vincenzorm117 <vincenzorm117@gmail.com>
2015-12-25 23:36:31 -0500

    Validated the check in page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,62 @@
 
 $(document).ready(function() {
 	setTimeout(function() {
-		$('.alert').fadeOut(650);
+		$('.alert-success').fadeOut(650);
 	}, 500);
+
+
+	$('[action="/visits"]').submit(function(e){
+		var fist, last, employee;
+		try {
+			first = $('#first_name').val();
+			last = $('#last_name').val();
+			employee = $('#employee').val();
+		}catch(ex) {
+			console.error('Error:!')
+			e.preventDefault();
+		}
+
+		var bool = {
+			first: first.length <= 0,
+			last: last.length <= 0,
+			employee: employee.length <= 0
+		}
+
+		$('.panel-new-visitor').find('.message-error').remove();
+
+
+		if( bool.first || bool.last || bool.employee ) {
+
+			if( bool.first ) {
+				$('#first_name').after($('<div>', {
+					'class': 'message-error',
+					'html': 'First name required'
+				}));
+			}
+			if( bool.last ) {
+				$('#last_name').after($('<div>', {
+					'class': 'message-error',
+					'html': 'Last name required'
+				}));
+			}
+			if( bool.employee ) {
+				$('#employee').after($('<div>', {
+					'class': 'message-error',
+					'html': 'Alaso Employee required'
+				}));
+			}
+			e.preventDefault();
+		} 
+	});
 });
+
+
+
+
+
+
+
+
+
+
+
