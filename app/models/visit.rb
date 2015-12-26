1d640a14658378df5d0f24859ccd0667b13fa7f1
Alex Bake <abake@enova.com>
2015-12-26 00:04:37 -0500

    Added duration

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,4 +1,9 @@
 class Visit < ActiveRecord::Base
   belongs_to :visitor
   validates :visitor_id, presence: true
+
+  def duration
+    return if checked_out_at.nil?
+    checked_out_at - created_at
+  end
 end
